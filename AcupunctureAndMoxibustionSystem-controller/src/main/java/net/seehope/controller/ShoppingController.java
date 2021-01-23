5f11d99008d73e146d80aea59736fb6d92955e2a
everLuck666 <236356008@qq.com>
2021-01-23 21:20:21 +0800

    sz第二次

--- AcupunctureAndMoxibustionSystem-controller/src/main/java/net/seehope/controller/ShoppingController.java
+++ AcupunctureAndMoxibustionSystem-controller/src/main/java/net/seehope/controller/ShoppingController.java
@@ -15,36 +15,7 @@ public class ShoppingController {
     @Autowired
     ShoppingService shoppingService;
 
-    @GetMapping("getAllGoods")
-    public RestfulJson getAllGoods(){
-        System.out.println("巴拉巴拉");
-        return RestfulJson.isOk(shoppingService.getAllGoods());
-    }
 
-    @GetMapping("getGoodsBy")
-    public RestfulJson getGoodsByKeyword(String keyword){
-        return RestfulJson.isOk(shoppingService.getGoodsByKeyword(keyword));
-    }
-
-    @PutMapping("addAddress")
-    public RestfulJson addAddress(@RequestBody UserInfo userInfo, HttpServletRequest request){
-        String userId = request.getAttribute("openId").toString();
-        userInfo.setUserId(userId);
-        try {
-            shoppingService.addAddress(userInfo);
-            return RestfulJson.isOk("添加地址成功！");
-        }catch (Exception e){
-            return RestfulJson.errorMsg(e.getMessage());
-        }
-
-    }
-
-    @GetMapping("getUserAddress")
-    public RestfulJson getUserAddress(HttpServletRequest request){
-        String userId = request.getAttribute("openId").toString();
-        return RestfulJson.isOk(shoppingService.getUserAddress(userId));
-
-    }
 
 
 
